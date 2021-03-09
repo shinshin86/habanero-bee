343c83d5b38c2d0006760aa7399cb0d201f2434e
shinshin86 <beagles1986@gmail.com>
2021-03-09 18:41:58 +0900

    Add test

---
+++ test/utils/slug.test.ts
@@ -0,0 +1,15 @@
+import { getSlugText } from '@/utils/slug';
+
+describe('getSlugText', (): void => {
+  test('Should return text all lowercase.', (): void => {
+    expect(getSlugText('TEST')).toBe('test');
+  });
+
+  test('Should return text all lowercase. (If the arguments is all lowercase)', (): void => {
+    expect(getSlugText('test')).toBe('test');
+  });
+
+  test('Should return empty string, if the arguments is empty string.', (): void => {
+    expect(getSlugText('')).toBe('');
+  });
+});
