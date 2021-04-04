53536a8a5ed22dccff087d5c4d12020eb2b39465
shinshin86 <beagles1986@gmail.com>
2021-04-04 10:26:22 +0900

    Add amp-accordion

--- src/components/RelatedContentList.tsx
+++ src/components/RelatedContentList.tsx
@@ -5,12 +5,29 @@ const RelatedContentList: React.FC<{
   relatedContentTitle: string;
 }> = ({ relatedContentList, relatedContentTitle }): JSX.Element => (
   <div>
-    <h2>{relatedContentTitle}</h2>
-    {relatedContentList.map((content) => (
-      <p key={content.slug}>
-        <a href={`/${content.slug}`}>{content.title}</a>
-      </p>
-    ))}
+    <amp-accordion disable-session-states="" animate="">
+      {/* @ts-ignore */}
+      <section expanded="">
+        {/* It didn't reflect, so I wrote it directly style. */}
+        <h2
+          style={{
+            borderRadius: 24,
+            margin: 16,
+            padding: '0 8px',
+            fontSize: '1.5em',
+          }}
+        >
+          {relatedContentTitle}
+        </h2>
+        <div>
+          {relatedContentList.map((content) => (
+            <p key={content.slug}>
+              <a href={`/${content.slug}`}>{content.title}</a>
+            </p>
+          ))}
+        </div>
+      </section>
+    </amp-accordion>
   </div>
 );
 
