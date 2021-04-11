9d7eb87d7f34774dc2a9ce071ac0b407eaf75ecf
shinshin86 <beagles1986@gmail.com>
2021-04-11 21:56:10 +0900

    Fix copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,43 @@
 const Footer: React.FC<{ link: string; text: string }> = ({
   link,
   text,
-}): JSX.Element => (
-  <footer id="footer">
-    <div>
-      <ul className="copyright">
-        <li>
-          &copy;{' '}
-          <a
-            href={link || 'https://github.com/shinshin86/habanero-bee'}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {text || 'Habanero Bee'}
-          </a>
-        </li>
-      </ul>
-    </div>
-  </footer>
-);
+}): JSX.Element => {
+  if (!text && !!link) {
+    throw new Error(
+      'BUILD ERROR: If you specify a link for copyright, please specify the text as well.'
+    );
+  }
+
+  /**
+   * Copyright link or text
+   * link: If both text and link are specified
+   * link: If both text and link are not specified
+   * text: If text only are specified
+   */
+  const isCopyrightLink = (!text && !link) || (!!text && !!link);
+
+  return (
+    <footer id="footer">
+      <div>
+        <ul className="copyright">
+          <li>
+            &copy;{' '}
+            {isCopyrightLink ? (
+              <a
+                href={link || 'https://github.com/shinshin86/habanero-bee'}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {text || 'Habanero Bee'}
+              </a>
+            ) : (
+              <>{text}</>
+            )}
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
