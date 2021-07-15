089a0895fac0d0f8cc8b1b75e9d6e4a871470bd1
shinshin86 <beagles1986@gmail.com>
2021-07-15 12:05:49 +0900

    Code format

--- test/utils/validate.test.ts
+++ test/utils/validate.test.ts
@@ -1,24 +1,23 @@
 import { isValidUrl } from '@/utils/validate';
 
 describe('isValidData', (): void => {
-    test('Should return true, If url is valid. (http)', (): void => {
-        const validURL = 'http://example.com';
-        expect(isValidUrl(validURL)).toBeTruthy();
-    })
+  test('Should return true, If url is valid. (http)', (): void => {
+    const validURL = 'http://example.com';
+    expect(isValidUrl(validURL)).toBeTruthy();
+  });
 
-    test('Should return true, If url is valid. (https)', (): void => {
-        const validURL = 'https://example.com';
-        expect(isValidUrl(validURL)).toBeTruthy();
-    })
+  test('Should return true, If url is valid. (https)', (): void => {
+    const validURL = 'https://example.com';
+    expect(isValidUrl(validURL)).toBeTruthy();
+  });
 
-    test('Should return false, if url is invalid', (): void => {
-        const invalidURL = 'htt://example.com';
-        expect(isValidUrl(invalidURL)).toBeFalsy();
-    })
+  test('Should return false, if url is invalid', (): void => {
+    const invalidURL = 'htt://example.com';
+    expect(isValidUrl(invalidURL)).toBeFalsy();
+  });
 
-    test('Should return false, if url is empty', (): void => {
-        const emptyURL = '';
-        expect(isValidUrl(emptyURL)).toBeFalsy();
-    })
-})
-  
+  test('Should return false, if url is empty', (): void => {
+    const emptyURL = '';
+    expect(isValidUrl(emptyURL)).toBeFalsy();
+  });
+});
